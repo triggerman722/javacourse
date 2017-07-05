9b5e9c61eeed58dc362da95583fa874f55de7631
greg <gemartin@gmail.com>
2017-07-04 20:37:22 -0400

    new comment

--- 04-5-MainMethod/src/com/java9/Main.java
+++ 04-5-MainMethod/src/com/java9/Main.java
@@ -2,6 +2,7 @@ package com.java9;
 
 public class Main {
 
+    //"main" is the main "entrypoint" to a java application.
     public static void main(String[] args) {
         System.out.println("Hello Everybody!");
     }
